14497b056a30169b34dca879e0ce0b8f2621a1ac
bruna markowisk <bruninha.markowisk@hotmail.com>
2024-12-14 10:20:11 -0300

    new: configurations packages

---
+++ backend/src/main/java/com/workloom/workloom_plataform/WorkloomPlataformApplication.java
@@ -0,0 +1,13 @@
+package com.workloom.workloom_plataform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloomPlataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloomPlataformApplication.class, args);
+	}
+
+}
