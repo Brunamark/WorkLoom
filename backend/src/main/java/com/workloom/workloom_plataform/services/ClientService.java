acca75938312efa24c11df6fe276956c67625c6d
bruna markowisk <bruninha.markowisk@hotmail.com>
2025-01-27 22:42:28 -0300

    fix: createClient and add editAddress

--- backend/src/main/java/com/workloom/workloom_plataform/services/ClientService.java
+++ backend/src/main/java/com/workloom/workloom_plataform/services/ClientService.java
@@ -2,13 +2,17 @@ package com.workloom.workloom_plataform.services;
 
 import com.workloom.workloom_plataform.domain.users.client.Client;
 import com.workloom.workloom_plataform.domain.users.client.ClientRequestDTO;
+import com.workloom.workloom_plataform.domain.users.embedded.Address;
+import com.workloom.workloom_plataform.domain.users.embedded.AddressDTO;
 import com.workloom.workloom_plataform.domain.users.skill.Skill;
 import com.workloom.workloom_plataform.repository.ClientRepository;
 import com.workloom.workloom_plataform.repository.SkillRepository;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -20,20 +24,31 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    public Client createClient(ClientRequestDTO data) {
-        Client client = new Client();
-        client.setName(data.getName());
-        client.setEmail(data.getEmail());
-        client.setPassword(data.getPassword());
-        client.setCnpj(data.getCnpj());
-        client.setRole(data.getRole());
-        client.setEnterpriseName(data.getEnterpriseName());
-        client.setSkills(data.getSkills());
-        ///client.setStreetName(data.getStreetName());
+    public Client createClient(Client client) {
+        Client newClient = new Client();
+        newClient.setName(client.getName());
+        newClient.setEmail(client.getEmail());
+        newClient.setCnpj(client.getCnpj());
+        newClient.setRole(client.getRole());
+        newClient.setEnterpriseName(client.getEnterpriseName());
+        newClient.setSkills(client.getSkills());
+        
 
+        List<Address> addresses = newClient.getAddress().stream().map(dto -> {
+            Address address = new Address();
+            address.setStreetName(dto.getStreetName());
+            address.setStreetNumber(dto.getStreetNumber());
+            address.setCity(dto.getCity());
+            address.setState(dto.getState());
+            address.setZipCode(dto.getZipCode());
+            return address;
+        }).collect(Collectors.toList());
+    
+        newClient.setAddress(addresses);
 
-        return clientRepository.save(client);
+        return clientRepository.save(newClient);
     }
+
     private Client findClientById(UUID clientId) {
         return clientRepository.findById(clientId)
                 .orElseThrow(() -> new RuntimeException("Client not found!"));
@@ -68,7 +83,7 @@ public class ClientService {
         if (client.getName() == null) {
             throw new IllegalArgumentException("Enterprise name do not exist");
         }
-        
+
         client.setEnterpriseName(newEnterpriseName);
         return client;
     }
@@ -81,7 +96,6 @@ public class ClientService {
         client.setCnpj(cnpj);
         return client;
     }
-
 
     public Client editCnpj(UUID clientId, String newCnpj) {
         Client client = findClientById(clientId);
@@ -108,9 +122,27 @@ public class ClientService {
 
     public void editClientSkill(UUID clientId, UUID oldSkillId, UUID newSkillId) {
         Client client = clientRepository.findById(clientId).orElseThrow(() -> new RuntimeException("Client not found"));
-        Skill oldSkill = skillRepository.findById(oldSkillId).orElseThrow(() -> new RuntimeException("Old skill not found"));
-        Skill newSkill = skillRepository.findById(newSkillId).orElseThrow(() -> new RuntimeException("New skill not found"));
+        Skill oldSkill = skillRepository.findById(oldSkillId)
+                .orElseThrow(() -> new RuntimeException("Old skill not found"));
+        Skill newSkill = skillRepository.findById(newSkillId)
+                .orElseThrow(() -> new RuntimeException("New skill not found"));
         client.editSkill(oldSkill, newSkill);
         clientRepository.save(client);
+    }
+
+    public Client editAddresses(UUID clientId, List<AddressDTO> addressDTOs) {
+        Client client = findClientById(clientId);
+        List<Address> addresses = addressDTOs.stream().map(dto -> {
+            Address address = new Address();
+            address.setStreetName(dto.getStreetName());
+            address.setStreetNumber(dto.getStreetNumber());
+            address.setCity(dto.getCity());
+            address.setState(dto.getState());
+            address.setZipCode(dto.getZipCode());
+            return address;
+        }).collect(Collectors.toList());
+
+        client.setAddress(addresses);
+        return clientRepository.save(client);
     }
 }
